7cd9ee8d40ba9b6d115e5ab55eb2f7f5dec19ef6
Jaya Kasa <kasa288@gmail.com>
2016-07-09 19:21:54 -0400

    Finished Solution
    Main method is in Maze class

--- src/Maze/Maze.java
+++ src/Maze/Maze.java
@@ -1,5 +1,13 @@
 package Maze;
 
+import Util.MazeNode;
+import Util.MazeReader;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Class finds a path from the start position to the goal position in the maze using AStar search.
  *
@@ -7,7 +15,150 @@ package Maze;
  * @version 1.0
  */
 public class Maze {
+    private MazeNode[][] maze = null;
+    private int startx = 0;
+    private int starty = 0;
+    private int finishx = 0;
+    private int finishy = 0;
+    private Deque<MazeNode> open = new LinkedList<>();
+    private Stack<MazeNode> close = new Stack<>();
 
+    Maze(String filename){
+        maze = MazeReader.readFile(filename);
+        finishx = maze.length-1;
+        finishy = maze[finishx].length-1;
+    }
+
+    Maze(String filename, int finishx, int finishy){
+        maze = MazeReader.readFile(filename);
+
+        int maxx = maze.length-1;
+        int maxy = maze[finishx].length-1;
+
+        if(finishx > 0 && finishx <= maxx){
+            this.finishx = finishx;
+        }
+        else{
+            throw new IllegalArgumentException("Goal x Position is out of bounds of maze.");
+        }
+
+        if(finishy > 0 && finishy <= maxy){
+            this.finishy = finishy;
+        }
+        else{
+            throw new IllegalArgumentException("Goal y Position is out of bounds of maze.");
+        }
+    }
+
+    /**
+     * Method to solve the maze using AStar Algorithm
+     */
+    void solve(){
+        open.add(maze[startx][starty]);
+        while(!open.isEmpty()){
+            MazeNode curr = open.pop();
+            int x = curr.getX();
+            int y = curr.getY();
+            if(x == finishx && y == finishy){
+                return;
+            }
+            close.add(curr);
+            short nullcount = 0;
+            MazeNode[] neighbors = getNeighbors(curr);
+            for(MazeNode n : neighbors){
+                if(n == null){
+                    continue;
+                }
+                if(!open.contains(n)){
+                    n.setParent(curr);
+                    n.setG(curr.getG() + 1);
+                    n.setF(h(n.getX(), n.getY()));
+                    open.add(n);
+                }
+                else{
+                    int g = curr.getG() + 1;
+                    double h = h(n.getX(), n.getY());
+                    if(n.getF() < h+g){
+                        n.setParent(curr);
+                        n.setG(g);
+                        n.setF(h);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Method to print the path solution for the maze from the given starting positions
+     *
+     * @param x
+     * @param y
+     */
+    void printPath(short x, short y){
+        this.startx = x;
+        this.starty = y;
+        solve();
+    }
+
+    /**
+     * Method to print the path solution for the maze from the given starting positions
+     */
+    void printPath(){
+        if(maze[finishx][finishy] == null){
+            System.err.println("Goal Position is an obstable! No legal path to goal exists!");
+            return;
+        }
+        solve();
+        Stack<MazeNode> path = new Stack<>();
+        MazeNode curr = close.pop();
+        while(curr.compareTo(maze[startx][starty]) != 0){
+            path.add(curr);
+            curr = curr.getParent();
+        }
+        path.add(curr);
+        System.out.println("Path for current maze took " + path.size() + " moves.");
+        while(!path.empty()){
+            System.out.println(path.pop());
+        }
+    }
+
+    /**
+     * Method gets the current nodes neighbors and excludes the parents and any nodes already in the path
+     * @param curr
+     * @return
+     */
+    private MazeNode[] getNeighbors(MazeNode curr){
+        //calculate the neighbors
+        int x = curr.getX();
+        int y = curr.getY();
+        MazeNode[] neighbors = new MazeNode[4];
+        if(x-1 > 0){
+            neighbors[0] = maze[x-1][y];
+            if(curr != null && neighbors[0] != null && (curr.getParent() != null && neighbors[0].compareTo(curr.getParent()) == 0) || close.contains(neighbors[0])){
+                neighbors[0] = null;
+            }
+        }
+        if(x+1 < maze.length){
+            neighbors[1] = maze[x+1][y];
+            if(curr != null && neighbors[1] != null && (curr.getParent() != null && neighbors[1].compareTo(curr.getParent()) == 0) || close.contains(neighbors[1])){
+                neighbors[1] = null;
+            }
+        }
+        if(y-1 > 0){
+            neighbors[2] = maze[x][y-1];
+            if(curr != null && neighbors[2] != null && (curr.getParent() != null && neighbors[2].compareTo(curr.getParent()) == 0) || close.contains(neighbors[2])){
+                neighbors[2] = null;
+            }
+        }
+        if(y+1 < maze[x].length){
+            neighbors[3] = maze[x][y+1];
+            if(curr != null && neighbors[3] != null && (curr.getParent() != null && neighbors[3].compareTo(curr.getParent()) == 0) || close.contains(neighbors[3])){
+                neighbors[3] = null;
+            }
+        }
+
+        return neighbors;
+    }
 
     /**
      * Hueristic Evaluation function
@@ -15,30 +166,15 @@ public class Maze {
      * @param x
      * @param y
      */
-    private void f(int x, int y){
-        //todo implement
+    private double h(int x, int y){
         //consider merging the h and g methods into this one
+        return Math.sqrt((finishx - x)*(finishx - x) + (finishy - y)*(finishy - y));
     }
 
-    /**
-     * Hueristic Function uses Manhattan distance formula
-     *
-     * @param x
-     * @param y
-     */
-    private void h(int x, int y){
-        //todo implement
+    public static void main(String ... args){
+        new Maze("multipath").printPath();
+        new Maze("smallmaze").printPath();
+        new Maze("maze_No_1.txt").printPath();
+        new Maze("maze_No_2.txt").printPath();
     }
-
-    /**
-     * Calculates cost from start to finish
-     *
-     * @param x
-     * @param y
-     */
-    private void g(int x, int y){
-        //todo implement
-    }
-
-
 }
--- src/Util/MazeNode.java
+++ src/Util/MazeNode.java
@@ -6,12 +6,14 @@ package Util;
  * @author Jaya Kasa
  * @version 1.0
  */
-public class MazeNode {
-    private short x;
-    private short y;
+public class MazeNode implements Comparable{
+    private final int x;
+    private final int y;
+    private int g = 0;
+    private double f = 0;
     private MazeNode parent = null;
 
-    public MazeNode(short x, short y){
+    public MazeNode(int x, int y){
         this.x = x;
         this.y = y;
     }
@@ -21,21 +23,21 @@ public class MazeNode {
      *
      * @return
      */
-    public short getX(){ return x; }
+    public int getX(){ return x; }
 
     /**
      * Get the y value
      *
      * @return
      */
-    public short getY(){ return y; }
+    public int getY(){ return y; }
 
     /**
      * On Visit, update the parent of the curret node to the node from which the current node was first visited.
      *
      * @param parent
      */
-    public void onVisit(MazeNode parent){
+    public void setParent(MazeNode parent){
         this.parent = parent;
     }
 
@@ -52,4 +54,59 @@ public class MazeNode {
      * Method resets the parent to null if current node is deemed unnecessary
      */
     public void resetParent(){ parent = null; }
+
+    /**
+     * Method to set the G value for the node
+     * @param g
+     */
+    public void setG(int g){
+        this.g = g;
+    }
+
+    /**
+     * Get the G value for the current node
+     * @return
+     */
+    public int getG(){
+        return g;
+    }
+
+    /**
+     * Method to set the F value for the node
+     * @param f
+     */
+    public void setF(double f){
+        this.f = f + g;
+    }
+
+    /**
+     * Get the F value for the current node
+     * @return
+     */
+    public double getF(){
+        return f;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(o == null){
+            return -1;
+        }
+        else if(!(o instanceof MazeNode)){
+            return -1;
+        }
+        MazeNode m = (MazeNode)o;
+        if(m.getX() > x || m.getY() > y){
+            return 1;
+        }
+        if(m.getX() == x && m.getY() == y){
+            return 0;
+        }
+        return -1;
+    }
 }
